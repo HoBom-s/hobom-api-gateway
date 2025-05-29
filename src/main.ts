de55f32dd8cbbeefc6082edf18ebca82403a5a77
foxmon <jjockrod@naver.com>
2025-05-30 00:53:33 +0900

    [KAN-68] gateway

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import * as express from "express";
 import { AppModule } from "./app.module";
 import { RequestIdMiddleware } from "./shared/middlewares/request-id.middleware";
 import { TraceInterceptor } from "./shared/interceptors/trace.interceptors";
@@ -6,6 +7,25 @@ import { TraceInterceptor } from "./shared/interceptors/trace.interceptors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      process.env.HOBOM_CLIENT_HOST_LOCAL,
+      process.env.HOBOM_CLIENT_HOST_LIVE,
+    ],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Credentials",
+    ],
+    credentials: true,
+  });
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(new RequestIdMiddleware().use);
 
   app.useGlobalInterceptors(new TraceInterceptor());
@@ -13,4 +33,8 @@ async function bootstrap() {
   await app.listen(process.env.HOBOM_API_GATEWAY_PORT ?? 8080);
 }
 
-bootstrap().then(() => console.log("HoBom API GATEWAY Listening 🦊🐻"));
+bootstrap().then(() =>
+  console.log(
+    `HoBom API GATEWAY Listening 🦊🐻 PORT: ${process.env.HOBOM_API_GATEWAY_PORT}`,
+  ),
+);
