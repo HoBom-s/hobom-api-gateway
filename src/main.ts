1de665778ee4e84b0f81ec1cfcdceac4cdb171f8
foxmon <jjockrod@naver.com>
2025-05-30 18:54:46 +0900

    port binding

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new TraceInterceptor());
 
-  await app.listen(process.env.PORT || 8080, () => {
+  await app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
     console.log(`HoBom API GATEWAY Listening 🦊🐻 PORT: ${process.env.PORT}`);
   });
 }
