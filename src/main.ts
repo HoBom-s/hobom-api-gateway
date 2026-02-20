02729f2af6e0ec09633aebfffc286043858cce7c
foxmon <jjockrod@naver.com>
2026-02-20 18:34:28 +0900

    refactor

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json({ limit: "20mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "20mb" }));
   app.use(cookieParser());
   app.use(new RequestIdMiddleware().use);
 
